a6cbcb2305f9ae89b3d2281edf7c439c59c02c70
whj <1469202826@qq.com>
2020-08-25 22:28:19 +0800

    qw

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,11 +7,7 @@ module.exports = {
       { text: 'css', link: '/css/style' },
       { text: 'js', link: '/js/about' },
       { text: 'Vue', link: '/vue/vue' },
-<<<<<<< HEAD
-      { text: 'git', link: '/git/git' },
-=======
-      { text: '其他', items: [{ text: 'git', link: '/git/git' }] },
->>>>>>> 12552124a01d5599ef791fcca0408e090b81920f
+            { text: '其他', items: [{ text: 'git', link: '/git/git' }] },
       { text: 'GtHub', link: 'https://github.com/WangHaijian180/acolasia' },
     ]
   },
