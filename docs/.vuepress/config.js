907846400d9cac6b1fd0269cb3d61a76678e6cb6
whj <1469202826@qq.com>
2020-08-25 22:47:16 +0800

    12

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,26 @@
+const moment = require('moment');
+
 module.exports = {
   title: 'ACOLASIA',
+  plugins: [
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp) => {
+          moment.locale("zh-cn")
+          return moment(timestamp).format("LLLL")
+        }
+      }
+    ]
+  ],
   themeConfig: {
     logo: '/assets/img/logosy.jpg',
-    lastUpdated: 'Last Updated',
+    lastUpdated: '更新时间',
     nav: [
       { text: 'css', link: '/css/style' },
       { text: 'js', link: '/js/about' },
       { text: 'Vue', link: '/vue/vue' },
-            { text: '其他', items: [{ text: 'git', link: '/git/git' }] },
+      { text: '其他', items: [{ text: 'git', link: '/git/git' },{text:'Moment',link:'http://momentjs.cn'}] },
       { text: 'GtHub', link: 'https://github.com/WangHaijian180/acolasia' },
     ]
   },
