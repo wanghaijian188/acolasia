a1f39d1b78742e3cce7ce0d27ec2b0d4ec75eb71
unknown <1469202826@qq.com>
2020-09-17 18:18:42 +0800

    12

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,8 +22,14 @@ module.exports = {
       { text: 'Vue', link: '/vue/vue' },
       { text: '后端', items: [{ text: 'java', link: '/java/java' }] },
       { text: '数据库', link: '/vue/vue' },
-      { text: '其它', items: [{ text: 'vueCLI', link: '/vue/vueCLI' }, { text: '微信小程序', link: '/wx/wx' }, { text: 'git', link: '/git/git' }, { text: 'gspa', link: 'https://greensock.com/' }, { text: 'Moment', link: 'http://momentjs.cn' }] },
+      { text: '其它', items: [{ text: 'vueCLI', link: '/vue/vueCLI' }, { text: '宝典', link: '/bible/vue' }, { text: '微信小程序', link: '/wx/wx' }, { text: 'git', link: '/git/git' }, { text: 'gspa', link: 'https://greensock.com/' }, { text: 'Moment', link: 'http://momentjs.cn' }] },
       { text: 'GtHub', link: 'https://github.com/WangHaijian180/acolasia' },
+    ],
+    sidebar: [
+      'bible/vue',
+      'bible/js',
+      'bible/css',
+      'bible/Statuscode'
     ]
   },
 
