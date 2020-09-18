05c7316c0a8f88425125b3a5a33b1d44f41f392d
王海健 <1469202826@qq.com>
2020-09-18 08:06:43 +0800

    23

--- docs/.vuepress/dist/assets/js/3.3ae9385c.js
+++ docs/.vuepress/dist/assets/js/3.3ae9385c.js
@@ -1,15 +1,1 @@
-<<<<<<< HEAD
-<<<<<<<< HEAD:docs/.vuepress/dist/assets/js/3.3ae9385c.js
 (window.webpackJsonp=window.webpackJsonp||[]).push([[3],{317:function(t,e,n){},348:function(t,e,n){"use strict";var i=n(317);n.n(i).a},364:function(t,e,n){"use strict";n.r(e);var i={functional:!0,props:{type:{type:String,default:"tip"},text:String,vertical:{type:String,default:"top"}},render:function(t,e){var n=e.props,i=e.slots;return t("span",{class:["badge",n.type],style:{verticalAlign:n.vertical}},n.text||i().default)}},r=(n(348),n(42)),a=Object(r.a)(i,void 0,void 0,!1,null,"15b7b770",null);e.default=a.exports}}]);
-========
-=======
->>>>>>> 478c986d5002ea0c3b4ab17a9c57c5ad6e9cff7c
-<<<<<<< HEAD:docs/.vuepress/dist/assets/js/3.3ae9385c.js
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{317:function(t,e,n){},348:function(t,e,n){"use strict";var i=n(317);n.n(i).a},364:function(t,e,n){"use strict";n.r(e);var i={functional:!0,props:{type:{type:String,default:"tip"},text:String,vertical:{type:String,default:"top"}},render:function(t,e){var n=e.props,i=e.slots;return t("span",{class:["badge",n.type],style:{verticalAlign:n.vertical}},n.text||i().default)}},r=(n(348),n(42)),a=Object(r.a)(i,void 0,void 0,!1,null,"15b7b770",null);e.default=a.exports}}]);
-=======
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{317:function(t,e,n){},348:function(t,e,n){"use strict";var i=n(317);n.n(i).a},358:function(t,e,n){"use strict";n.r(e);var i={functional:!0,props:{type:{type:String,default:"tip"},text:String,vertical:{type:String,default:"top"}},render:function(t,e){var n=e.props,i=e.slots;return t("span",{class:["badge",n.type],style:{verticalAlign:n.vertical}},n.text||i().default)}},r=(n(348),n(42)),a=Object(r.a)(i,void 0,void 0,!1,null,"15b7b770",null);e.default=a.exports}}]);
->>>>>>> 16cc3c140d17c9ac2a3a0980deee8d52f62de913:docs/.vuepress/dist/assets/js/3.54d3a012.js
-<<<<<<< HEAD
->>>>>>>> 478c986d5002ea0c3b4ab17a9c57c5ad6e9cff7c:docs/.vuepress/dist/assets/js/3.54d3a012.js
-=======
->>>>>>> 478c986d5002ea0c3b4ab17a9c57c5ad6e9cff7c
