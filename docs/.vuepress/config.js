57ea5cfc8fecbd89462e7739d34df9e73edc1d10
王海健 <1469202826@qq.com>
2020-09-25 23:57:20 +0800

    12

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,21 +18,28 @@ module.exports = {
     lastUpdated: '更新时间',
     nav: [
       { text: 'css', link: '/css/style' },
-      { text: 'js', items: [{ text: 'javaScript', link: '/js/about' }, { text: '时间', link: '/date/date' }] },
+      { text: 'js', link: '/js/js' },
       { text: 'Vue', link: '/vue/vue' },
-      { text: '后端', items: [{ text: 'java', link: '/java/java' }] },
+      { text: '后端', items: [{ text: 'java', link: '/js/javascript' }] },
       { text: '数据库', link: '/vue/vue' },
       { text: '其它', items: [{ text: 'vueCLI', link: '/vue/vueCLI' }, { text: '宝典', link: '/bible/vue' }, { text: '微信小程序', link: '/wx/wx' }, { text: 'git', link: '/git/git' }, { text: 'gspa', link: 'https://greensock.com/' }, { text: 'Moment', link: 'http://momentjs.cn' }] },
       { text: 'GtHub', link: 'https://github.com/WangHaijian180/acolasia' },
     ],
-    sidebar: [
-      'bible/vue',
-      'bible/js',
-      'bible/css',
-      'bible/ajax',
-      'bible/Statuscode',
-      'bible/Other'
-    ]
+    sidebar: {
+      '/bible/': [
+        'vue',
+        'js',
+        'css',
+        'ajax',
+        'Statuscode',
+        'Other'
+      ],
+      '/js/': [
+        'js',
+        'Stringmethod',
+        'date'
+      ]
+    }
   },
 
 
