7b0a7a8aa0398c8b5da659baca0c3a7c48bd2b9b
whj <1469202826@qq.com>
2020-12-22 18:41:13 +0800

    12

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,7 +20,7 @@ module.exports = {
       { text: 'css', link: '/css/style' },
       { text: 'js', link: '/js/js' },
       { text: 'Vue', link: '/vue/vue' },
-      { text: '后端', items: [{ text: 'java', link: '/js/javascript' }] },
+      { text: '后端', items: [{ text: 'java', link: '/java/java' }] },
       { text: '数据库', link: '/vue/vue' },
       { text: '其它', items: [{ text: 'vueCLI', link: '/vue/vueCLI' }, { text: '朵朵', link: '/duoduo/duoduo' }, { text: '宝典', link: '/bible/vue' }, { text: '微信小程序', link: '/wx/wx' }, { text: 'git', link: '/git/git' }, { text: 'gspa', link: 'https://greensock.com/' }, { text: 'Moment', link: 'http://momentjs.cn' }] },
       { text: 'GtHub', link: 'https://github.com/WangHaijian180/acolasia' },
