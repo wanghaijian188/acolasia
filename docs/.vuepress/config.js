0b5a4ecd0f529506eec04a1d26ef1f005fe2e5b5
whj <1469202826@qq.com>
2021-09-18 14:10:15 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,12 +17,13 @@ module.exports = {
     logo: '/assets/img/logosy.jpg',
     lastUpdated: '更新时间',
     nav: [
-      { text: 'css', link: '/css/style' },
-      { text: 'js', link: '/js/js' },
       { text: 'Vue', link: '/vue/vue' },
+      { text: 'js', link: '/js/js' },
+      { text: 'css', link: '/css/style' },
+      { text: 'uniapp', link: '/uniapp/uniapp'},
       { text: '后端', items: [{ text: 'java', link: '/java/java' }] },
       { text: '数据库', link: '/vue/vue' },
-      { text: '其它', items: [{ text: 'vueCLI', link: '/vue/vueCLI' }, { text: '朵朵', link: '/duoduo/duoduo' }, { text: '宝典', link: '/bible/vue' }, { text: '微信小程序', link: '/wx/wx' }, { text: 'git', link: '/git/git' }, { text: 'gspa', link: 'https://greensock.com/' }, { text: 'Moment', link: 'http://momentjs.cn' }] },
+      { text: '其它', items: [, { text: '宝典', link: '/bible/vue' }, { text: '微信小程序', link: '/wx/wx' }, { text: 'git', link: '/git/git' }, { text: 'gspa', link: 'https://greensock.com/' }, { text: 'Moment', link: 'http://momentjs.cn' }] },
       { text: 'GtHub', link: 'https://github.com/WangHaijian180/acolasia' },
     ],
     sidebar: {
@@ -40,11 +41,7 @@ module.exports = {
         'ArrayMethod',
         'date',
         'javascript'
-      ],
-      '/duoduo/': [
-        'duoduo',
-        'fuwuq'
-      ],
+      ]
     }
   },
 
---
+++ docs/uniapp/uniapp.md
@@ -0,0 +1,10 @@
+---
+sidebar: auto
+---
+
+```
+    <script src="https://cdn.bootcss.com/eruda/1.4.3/eruda.min.js"></script>
+    <script>
+        eruda.init()
+    </script>
+```
